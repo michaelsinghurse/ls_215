74c9a612be13b70e277c3aab1ed8eace90524f01
Michael Singhurse <michaelsinghurse@gmail.com>
2020-05-05 11:31:41 -0500

    completed exercise

--- exercises/list_processing/inventory_item_availability.js
+++ exercises/list_processing/inventory_item_availability.js
@@ -1,7 +1,18 @@
-// inventory_items.js
+// inventory_item_availability.js
 
 function transactionsFor(inventoryItem, transactions) {
   return transactions.filter(transaction => transaction.id === inventoryItem);
+}
+
+function isItemAvailable(itemId, transactions) {
+  let items = transactionsFor(itemId, transactions);
+
+  let quantity = items.reduce((total, item) => {
+    total += (item.movement === "in") ? item.quantity : -1 * item.quantity;
+    return total;
+  }, 0);
+
+  return quantity > 0;
 }
 
 var transactions = [ { id: 101, movement: 'in',  quantity:  5 },
@@ -15,8 +26,5 @@ var transactions = [ { id: 101, movement: 'in',  quantity:  5 },
                      { id: 102, movement: 'in',  quantity: 22 },
                      { id: 103, movement: 'out', quantity: 15 }, ];
 
-console.log(transactionsFor(101, transactions));
-// returns
-//[ { id: 101, movement: "in",  quantity:  5 },
-//  { id: 101, movement: "in",  quantity: 12 },
-//  { id: 101, movement: "out", quantity: 18 }, ]
+console.log(isItemAvailable(101, transactions) === false);
+console.log(isItemAvailable(105, transactions) === true);
